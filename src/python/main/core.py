902070ca3aec51bdd8d79c8d284c7b7ed25ae40e
ConcurrencyPractitioner <yohan.richard.yu@gmail.com>
2020-04-07 16:51:52 -0700

    Add separate directory

---
+++ src/python/main/core.py
@@ -0,0 +1,7 @@
+import random
+
+#chooses a random message to send to the user
+def get_message():
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    # return selected item to the user
+    return random.choice(sample_responses)
